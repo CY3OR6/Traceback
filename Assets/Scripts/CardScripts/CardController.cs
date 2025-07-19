259b2354b72b534ec029aa8862892aed1e6596ee
CY3OR6 <mustafashakir43@gmail.com>
2025-07-19 23:55:08 +0530

    feat: implemented basic gameplay loop
    Set up the core gameplay loop for the card matching mechanic.
    Includes logic for card selection, matching validation, handling matched and unmatched cards

--- Assets/Scripts/CardScripts/CardController.cs
+++ Assets/Scripts/CardScripts/CardController.cs
@@ -1,12 +1,13 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
+using TMPro;
 
 public class CardController : MonoBehaviour, IPointerClickHandler
 {
-
+    [SerializeField]
+    string cardID;
 
     [SerializeField]
     GameObject cardFront;
@@ -14,8 +15,19 @@ public class CardController : MonoBehaviour, IPointerClickHandler
     [SerializeField]
     GameObject cardBack;
 
+    [SerializeField]
+    TMP_Text cardTextObject = null;
+
+    [SerializeField]
     bool isFlipped = false;
 
+    public bool canFlip { get; private set; }
+
+
+    private void Awake()
+    {
+        cardTextObject = cardFront.GetComponentInChildren<TMP_Text>();
+    }
 
 
     // Start is called before the first frame update
@@ -32,12 +44,22 @@ public class CardController : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        if (!canFlip)
+        {
+            return;
+        }
         FlipCard();
+        GameManager.SelectCard(this);
     }
 
 
     public void FlipCard()
     {
+        if (!canFlip)
+        {
+            return;
+        }
+
         isFlipped = !isFlipped;
         cardFront.transform.DOLocalRotate(
             new Vector3(0, !isFlipped ? 180 : 0, 0),
@@ -48,6 +70,42 @@ public class CardController : MonoBehaviour, IPointerClickHandler
            new Vector3(0, isFlipped ? 180 : 0, 0),
           0.5f
        ).SetEase(Ease.OutBack);
+    }
+
+    public void CardSetup()
+    {
+        cardTextObject.text = cardID;
+        canFlip = true;
+    }
+
+    public string GetCardID()
+    {
+        return cardID;
+    }
+
+    public void SetCardID(string id)
+    {
+        cardID = id;
+        CardSetup();
+        StartCoroutine(ShowCardAndReset());
+    }
+
+    IEnumerator ShowCardAndReset()
+    {
+        FlipCard();
+        yield return new WaitForSeconds(1f);
+        FlipCard();
+    }
+
+    public IEnumerator CardReset()
+    {
+        yield return new WaitForSeconds(0.25f);
+        FlipCard();
+    }
+
+    public void CardMatched()
+    {
+        canFlip = false;
     }
 
 }
---
+++ Assets/Scripts/CardScripts/GameManager.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+
+    [SerializeField]
+    List<string> cardIDs = new List<string>();
+
+    List<CardController> cards = new List<CardController>();
+
+    [SerializeField]
+    GameObject cardPrefab = null;
+
+    [SerializeField]
+    Transform cardParent = null;
+
+    [SerializeField]
+    CardController cardSelected1 = null;
+
+    [SerializeField]
+    CardController cardSelected2 = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        SetupGame();
+    }
+
+    private void Start()
+    {
+    }
+
+    public void SetupGame()
+    {
+        if (cards.Count > 0)
+        {
+            foreach (CardController card in cards)
+            {
+                Destroy(card.gameObject);
+            }
+            cards.Clear();
+        }
+
+        List<string> _tempCardId = cardIDs;
+
+        for (int i = cardIDs.Count - 1; i >= 0; i--)
+        {
+            _tempCardId.Add(cardIDs[i]);
+        }
+
+        int ri = Random.Range(0, _tempCardId.Count);
+
+
+        while (_tempCardId.Count > 0)
+        {
+            GameObject newCard = Instantiate(cardPrefab, cardParent);
+            newCard.GetComponent<CardController>().SetCardID(_tempCardId[ri]);
+            cards.Add(newCard.GetComponent<CardController>());
+            _tempCardId.RemoveAt(ri);
+            ri = Random.Range(0, _tempCardId.Count);
+        }
+    }
+
+    public static void SelectCard(CardController card)
+    {
+        if (instance.cardSelected1 == null)
+        {
+            instance.cardSelected1 = card;
+        }
+        else if (instance.cardSelected2 == null && instance.cardSelected1 != card)
+        {
+            instance.cardSelected2 = card;
+        }
+
+        if (instance.cardSelected1 != null && instance.cardSelected2 != null)
+        {
+            if (instance.cardSelected1.GetCardID() != instance.cardSelected2.GetCardID())
+            {
+                instance.StartCoroutine(instance.cardSelected1.CardReset());
+                instance.StartCoroutine(instance.cardSelected2.CardReset());
+
+                instance.cardSelected1 = null;
+                instance.cardSelected2 = null;
+            }
+            else
+            {
+                instance.cardSelected1.CardMatched();
+                instance.cardSelected2.CardMatched();
+
+                instance.cardSelected1 = null;
+                instance.cardSelected2 = null;
+            }
+        }
+
+        foreach (CardController item in instance.cards)
+        {
+            if (item.canFlip)
+            {
+                return;
+            }
+        }
+
+        Debug.Log("All cards matched!");
+    }
+
+}
